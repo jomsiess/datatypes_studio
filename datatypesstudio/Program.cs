5fea75c5e8b38548a056a8cc9748ce2c12bc2e88
jomsiess <jomsiess@gmail.com>
2020-10-01 10:29:49 -0500

    update

--- datatypesstudio/Program.cs
+++ datatypesstudio/Program.cs
@@ -6,7 +6,7 @@ namespace datatypesstudio
     {
         static void Main(string[] args)
         {
-            double area;
+   
             string input;
             double radius;
             double circumference;
@@ -20,20 +20,15 @@ namespace datatypesstudio
             radius = double.Parse(input); 
 
 
-            area = Math.PI * radius * radius;
-            Console.WriteLine("The area of a circle of radius " + radius + " is: " + Math.Round(area, 2));
+            Console.WriteLine("The area of a circle of radius " + radius + " is: " + Math.Round(Circles.GetArea(radius), 2));
+            Console.WriteLine("The circumference of a circle of radius " + radius + " is: " + Math.Round(Circles.GetCircumference(radius), 2));
+            Console.WriteLine("The diameter of a circle of radius " + radius + " is: " + Math.Round(Circles.GetDiameter(radius), 2));
 
-            circumference = 2 * Math.PI * radius;
-            diameter = 2 * radius;
-            Console.WriteLine("The circumference of a circle of radius " + radius + " is: " + Math.Round(circumference, 2));
-            Console.WriteLine("The diameter of a circle of radius " + radius + " is: " + diameter);
-
-            Console.WriteLine("How many miles to the gallon does your car get?"); 
+            Console.WriteLine("How many miles to the gallon does your car get?");
             input2 = Console.ReadLine();
             mpg = double.Parse(input2);
 
-            trip = circumference / mpg;
-            Console.WriteLine("You will use about " + Math.Round(trip, 2) + " gallon/s of gas on this trip."); 
+            //Console.WriteLine("You will use about " + Math.Round, 2)) + " gallon/s of gas on this trip.");
 
         }
     }
