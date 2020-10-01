f16ebbde3e3349a24d820db8b1c65865c61265fd
jomsiess <jomsiess@gmail.com>
2020-10-01 10:29:05 -0500

    added Circles

---
+++ datatypesstudio/Circles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace datatypesstudio
+{
+    public class Circles
+    {
+        public static double GetArea(double radius)
+        {
+            double area = Math.PI * radius * radius;
+            return area; 
+        }
+        public static double GetCircumference(double radius)
+        { 
+            double circumference = 2 * Math.PI* radius;
+            return circumference; 
+        }
+        public static double GetDiameter(double radius)
+        {
+            double diameter = 2 * radius;
+            return diameter; 
+
+        }
+        public static double GetTrip(double mpg, double circumference)
+        {
+            double trip = circumference / mpg;
+            return trip;
+        }
+    }
+    }
